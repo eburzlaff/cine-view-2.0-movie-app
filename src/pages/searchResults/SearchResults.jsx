c64dde92d6dbd15b4c56d62c6877d373837088d0
eburzlaff <du.di.99@hotmail.com>
2023-11-17 03:00:36 -0600

    Search Results page is fully responsive now

--- src/pages/searchResults/SearchResults.jsx
+++ src/pages/searchResults/SearchResults.jsx
@@ -18,9 +18,15 @@ function SearchResults() {
     const { searchTerm } = useParams();
     const [ data, setData ] = useState();
 
+    const [screenSize, setScreenSize] = useState(getScreenSize()); // Initialize with the initial screen size
+
     // Pagination
     const [currentPage, setCurrentPage] = useState(1);
     const [pageCount, setPageCount] = useState(0);
+
+    // React Paginate state vars
+    const [pageRangeDisplayed, setPageRangeDisplayed] = useState(0); // Number of page links to display in the middle
+    const [marginPagesDisplayed, setMarginPagesDisplayed] = useState(0); // Number of page links to display at the beginning and end
 
     const getMoviesBySearchTerm = async () => {
         try {
@@ -50,6 +56,47 @@ function SearchResults() {
     const handlePageChange = ({ selected }) => {
         setCurrentPage(selected + 1); // React Paginate uses 0-based index
     };
+
+    // Function to get the screen size
+    function getScreenSize() {
+        if (window.innerWidth >= 1024) {
+            return 'lg'; // Large screens
+        } else if (window.innerWidth >= 768) {
+            return 'md'; // Medium screens
+        } else if (window.innerWidth >= 640) {
+            return 'sm'; // Small screens 
+        } else {
+            return 'xs'; // Extra Small screens
+        }
+    }
+
+    useEffect(() => {
+        // Adjust pageRangeDisplayed and marginPagesDisplayed based on screen size
+        const handleResize = () => {
+            setScreenSize(getScreenSize());
+            if (window.innerWidth <= 390) {
+                setPageRangeDisplayed(1);
+                setMarginPagesDisplayed(1);
+            } else if (window.innerWidth > 390 && window.innerWidth <= 640) {
+                setPageRangeDisplayed(2);
+                setMarginPagesDisplayed(1);
+            } else {
+                setPageRangeDisplayed(5);
+                setMarginPagesDisplayed(1);
+            }
+        };
+    
+        // Initial adjustment on mount
+        handleResize();
+    
+        // Listen for window resize events
+        window.addEventListener('resize', handleResize);
+    
+        // Clean up the event listener on component unmount
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -85,12 +132,22 @@ function SearchResults() {
                             disabledClassName={"pagination__link--disabled"}
                             activeClassName={"pagination__link--active"}
                             forcePage={currentPage - 1}
+                            pageRangeDisplayed={pageRangeDisplayed}
+                            marginPagesDisplayed={marginPagesDisplayed}
                         />
                     </div>
 
-                    <div className='flex flex-wrap'>
+                    <div className='flex flex-col items-center md:items-start md:flex-row flex-wrap'>
                         {data?.results.map((movie, id) => (
-                            <MovieCard data={movie} key={id} styles={{width: `calc(20% - 20px)`, aspectRatio: `1/1.5`, marginBottom: '30px'}} />
+                            <MovieCard 
+                                data={movie} 
+                                key={id} 
+                                styles={{
+                                    width: getScreenSize() === 'lg' ? 'calc(20% - 20px)' : getScreenSize() === 'md' ? 'calc(33% - 20px)' : getScreenSize() === 'sm' ? '50%' : '70%', 
+                                    aspectRatio: `1/1.5`, 
+                                    marginBottom: '30px'
+                                }} 
+                            />
                         ))}
                     </div>
 
@@ -106,6 +163,8 @@ function SearchResults() {
                             disabledClassName={"pagination__link--disabled"}
                             activeClassName={"pagination__link--active"}
                             forcePage={currentPage - 1}
+                            pageRangeDisplayed={pageRangeDisplayed}
+                            marginPagesDisplayed={marginPagesDisplayed}
                         />
                     </div>
                     
